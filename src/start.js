cc7864ee1a03e76b8a9a5db4f7e24ad4c923253b
Jhonathan Tabares <tantabares@gmail.com>
2020-08-19 06:45:39 +0100

    Last tweaks

--- src/start.js
+++ src/start.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const parser = require('body-parser');
 const { delilahApi } = require('./endpoints');
+const swagger = require('swagger-ui-express');
+const apidoc = require('./../apidoc.json');
 
 process.env.NODE_ENV = 'development';
 
@@ -11,11 +13,9 @@ const server = express();
 server.use(parser.json());
 
 server.get('/test', (req, res) => {
-  return res
-    .status(200)
-    .json({ message: 'connection ok', data: {} });
+  return res.status(200).json({ message: 'connection ok', data: {} });
 });
-
+server.use('/apidoc', swagger.serve, swagger.setup(apidoc));
 server.use('/api/delilah', delilahApi());
 
 server.listen(global.config.node_port, () => {
